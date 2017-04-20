dc5a0360bb4cd79e441ed88f26f7d2de20618a57
Chris Stone <chstone@microsoft.com>
2017-04-20 11:12:44 -0400

    lint fixes

--- src/mock-luis-service.ts
+++ src/mock-luis-service.ts
@@ -4,14 +4,14 @@ import { API_VERSION } from './luis-client';
 export class MockLuisService {
   scope: nock.Scope;
 
-  constructor(private appId:string, private region = 'westus') {
+  constructor(private appId: string, private region = 'westus') {
     this.scope = nock(`https://${region}.api.cognitive.microsoft.com`);
   }
 
-  recognize(statusCode:number, query:string, body?:any, headers?:any): MockLuisService {
+  recognize(statusCode: number, query: string, body?: any, headers?: any): MockLuisService {
     this.scope = this.scope
       .get(`/luis/v${API_VERSION}/apps/${this.appId}`)
-      .query((params:any) => params.q === query)
+      .query((params: any) => params.q === query)
       .reply(statusCode, body, headers);
     return this;
   }
